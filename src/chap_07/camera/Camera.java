c4c104687e4bf53eda3f3e6cefe3f9076d512d5a
HeoGwangYoung <heogwangyoung@gmail.com>
2023-04-23 01:17:44 +0900

    class-overriding

--- src/chap_07/camera/Camera.java
+++ src/chap_07/camera/Camera.java
@@ -11,9 +11,11 @@ public class Camera { // 부모 클래스
         // 사진 촬영
         System.out.println(this. name + " : 사진을 촬영합니다");
     }
-
     public void recordVideo() {
         // 동영상 녹화
         System.out.println(this. name + " : 동영상을 녹화합니다");
+    }
+    public void showMainFeature() {
+        System.out.println(this.name + "의 주요 기능 : 사진촬영, 동영상녹화");
     }
 }
--- src/chap_07/camera/SpeedCam.java
+++ src/chap_07/camera/SpeedCam.java
@@ -8,8 +8,13 @@ public class SpeedCam extends Camera { // 자식 클래스
         // 속도 체크
         System.out.println("속도를 측정합니다");
     }
+    //@Override // 부모클래스에 오버라이딩될 메소드가 없으면 에러
     public void recognizeLicensePlate() {
         // 번호 인식
         System.out.println("차량번호를 인식합니다");
+    }
+    @Override // annotation
+    public void showMainFeature() {
+        System.out.println(this.name + "의 주요 기능 : 속도 측정, 번호 인식");
     }
 }
