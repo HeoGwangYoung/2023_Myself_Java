25bf317e8a8663cf5b3a51a9903ca06c722dd350
HeoGwangYoung <heogwangyoung@gmail.com>
2023-05-15 00:35:24 +0900

    익명 클래스

--- src/chap_10/_02_AnonymousClass.java
+++ src/chap_10/_02_AnonymousClass.java
@@ -16,6 +16,7 @@ public class _02_AnonymousClass {
             public void cook() {
                 System.out.println("집에서 만드는 동생표 군대리아");
                 System.out.println("재료 : 빵, 치킨패티, 양배추 샐러드, 딸기잼, 치즈, 삶은 계란");
+                System.out.println();
             }
         };
     }
